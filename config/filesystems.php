1fc701eee184e2494394eb31a1237b62d4b9a448
mrnajafi81 <mohammad81.najafi@gmail.com>
2023-07-04 18:18:54 +0330

    update time and upload path and lang config

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,6 +30,14 @@ return [
 
     'disks' => [
 
+        'custom_public' => [
+            'driver' => 'local',
+            'root' => public_path('/'),
+            'url' => env('APP_URL') . '/',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -39,7 +47,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
